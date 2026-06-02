584c470ab78a7153b763382b50a60bf88d61e8a5
Sean Lin <hylin827@gmail.com>
2026-06-02 11:46:19 +0800

    sample code the performs image registration

--- ch06/ch06_5.py
+++ ch06/ch06_5.py
@@ -3,7 +3,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 target = cv2.imread('map_010pb_left_0.jpg') 
-reference= cv2.imread('map_010pb_right_0.jpg')
+reference= cv2.imread('Image00002.jpg')
 # feature extraction (特徵提取/擷取)
 orb = cv2.ORB_create(nfeatures=1000)
 kp1, descriptor1 = orb.detectAndCompute(target,None)
@@ -13,7 +13,7 @@ bf = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE)
 matches = bf.match(descriptor1,descriptor2)
 # 依據最匹配結果進行排序
 matches = sorted(matches, key = lambda x:x.distance)
-candidate_count = 3
+candidate_count = 5
 result = cv2.drawMatches(target, kp1, reference, kp2, matches[:candidate_count], None)
 
 cv2.imshow("result", result)
--- ch06/ch06_5b.py
+++ ch06/ch06_5b.py
@@ -2,17 +2,18 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-target = cv2.imread('map_010pb_left_0.jpg') 
-reference= cv2.imread('map_010pb_right_0.jpg')
-
+target = cv2.imread('30084_0.jpg') 
+reference= cv2.imread('30085_0.jpg')
+# 特徵擷取
 sift = cv2.xfeatures2d.SIFT_create()
 kp1, descriptor1 = sift.detectAndCompute(target,None)
 kp2, descriptor2 = sift.detectAndCompute(reference,None)
-
+# 特徵比對(匹配)
 bf = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE)
 matches = bf.match(descriptor1,descriptor2)
 matches = sorted(matches, key = lambda x:x.distance)
-candidate_count = 50
+# 繪製最匹配的代表
+candidate_count = 15
 result = cv2.drawMatches(target, kp1, reference, kp2, matches[:candidate_count], None)
 
 cv2.imshow("result", result)
